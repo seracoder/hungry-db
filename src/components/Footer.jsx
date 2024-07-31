d83339a78127e69c755a5edd1e5f5dd35e200111
Md Hamim <41739275+seracoder@users.noreply.github.com>
2024-07-31 21:32:57 +0600

    Fix self github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="text-center py-5 mt-14 bg-primary-content font-semibold text-white">
-            Made By <a href="https:/hithub.com/seracoder"><span className="text-primary">@Md Hamim</span></a>
+            Made By <a href="https:/github.com/seracoder" target="_blank"><span className="text-primary">@Md Hamim</span></a>
         </div>
     )
 }
